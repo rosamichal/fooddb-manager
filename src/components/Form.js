896d9f1d436709f9933d72353c4f8530989834e7
Michał Rosa <michal.rosa@action.pl>
2024-12-14 13:19:10 +0100

    Changed fields order in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import { useState } from 'react';
-import toast from 'react-hot-toast';
-import './Form.css';
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import "./Form.css";
 
-const apiKey = process.env.REACT_APP_API_KEY || 'test';
+const apiKey = process.env.REACT_APP_API_KEY || "test";
 
 function Form() {
-    const [name, setName] = useState('');
-    const [protein, setProtein] = useState('');
-    const [fat, setFat] = useState('');
-    const [carbohydrates, setCarbohydrates] = useState('');
+    const [name, setName] = useState("");
+    const [protein, setProtein] = useState("");
+    const [fat, setFat] = useState("");
+    const [carbohydrates, setCarbohydrates] = useState("");
     const [isProcessing, setIsProcessing] = useState(false);
 
     const submitHandler = async (event) => {
@@ -20,62 +20,74 @@ function Form() {
                 name,
                 protein: parseFloat(protein),
                 fat: parseFloat(fat),
-                carbohydrates: parseFloat(carbohydrates)
+                carbohydrates: parseFloat(carbohydrates),
             };
-            const response = await axios.post(`https://fooddb-node.herokuapp.com/api/ingredients?apiKey=${apiKey}`, newIngredient);
-            console.log('OK :) ', response);
+            const response = await axios.post(
+                `https://fooddb-node.herokuapp.com/api/ingredients?apiKey=${apiKey}`,
+                newIngredient
+            );
+            console.log("OK :) ", response);
             toast.success(`Pomyślnie dodano skladnik '${name}'`);
 
-            setName('');
-            setProtein('');
-            setFat('');
-            setCarbohydrates('');
+            setName("");
+            setProtein("");
+            setFat("");
+            setCarbohydrates("");
         } catch (error) {
             console.log(error);
             toast.error(`Nie udało się dodać składnika '${name}'`);
         } finally {
             setIsProcessing(false);
         }
-    }
+    };
 
     return (
         <form className="Form" onSubmit={submitHandler}>
             <input
                 className="name-input"
                 placeholder="Nazwa składnika..."
-                onChange={event => setName(event.target.value)}
+                onChange={(event) => setName(event.target.value)}
                 required
                 value={name}
-                disabled={isProcessing} />
-            <label htmlFor="b-input">Białko:</label>
-            <input
-                id="b-input"
-                required type="number"
-                step="0.1"
-                placeholder="białko"
-                onChange={event => setProtein(event.target.value)}
-                value={protein}
-                disabled={isProcessing} />
+                disabled={isProcessing}
+            />
             <label htmlFor="t-input">Tłuszcz:</label>
             <input
                 id="t-input"
-                required type="number"
+                required
+                type="number"
                 step="0.1"
                 placeholder="tłuszcz"
-                onChange={event => setFat(event.target.value)}
+                onChange={(event) => setFat(event.target.value)}
                 value={fat}
-                disabled={isProcessing} />
+                disabled={isProcessing}
+            />
             <label htmlFor="w-input">Węglowodany:</label>
             <input
                 id="w-input"
-                required type="number"
+                required
+                type="number"
                 step="0.1"
                 placeholder="węglowodany"
-                onChange={event => setCarbohydrates(event.target.value)}
+                onChange={(event) => setCarbohydrates(event.target.value)}
                 value={carbohydrates}
-                disabled={isProcessing} />
+                disabled={isProcessing}
+            />
+            <label htmlFor="b-input">Białko:</label>
+            <input
+                id="b-input"
+                required
+                type="number"
+                step="0.1"
+                placeholder="białko"
+                onChange={(event) => setProtein(event.target.value)}
+                value={protein}
+                disabled={isProcessing}
+            />
             <div className="buttons-container">
-                <button disabled={isProcessing}>{isProcessing ? "Proszę czekaj..." : "Dodaj"}</button>
+                <button disabled={isProcessing}>
+                    {isProcessing ? "Proszę czekaj..." : "Dodaj"}
+                </button>
             </div>
         </form>
     );
